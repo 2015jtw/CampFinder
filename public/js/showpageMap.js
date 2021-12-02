f56865ffa3e354fa18fc51b0537204ac3105f293
Joshua Worden <joshuaworden@Joshuas-MacBook-Pro.local>
2021-12-02 14:05:16 -0500

    fixing popups

--- public/js/showpageMap.js
+++ public/js/showpageMap.js
@@ -9,13 +9,15 @@ const map = new mapboxgl.Map({
 
 map.addControl(new mapboxgl.NavigationControl());
 
-// popup
+// popup this is what needs editing
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
             .setHTML(
                 `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
+            .setHTML(
                 `<h4>${campground.price}</h4>`
             )
     )
